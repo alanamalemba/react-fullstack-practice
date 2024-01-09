8c40e6966483cb4b3ffcafd0f805d6e63438f8a9
Alan Amalemba <alanlsamalemba@gmail.com>
2024-01-09 23:12:41 +0300

    Worked on associations in sequelize, created comments table uzing sequelize and implemented post and get for comments

--- server/models/Posts.js
+++ server/models/Posts.js
@@ -14,5 +14,9 @@ module.exports = (sequelize, DataTypes) => {
     },
   });
 
+  Posts.associate = (models) => {
+    Posts.hasMany(models.Comments, { onDelete: "cascade" });
+  };
+
   return Posts;
 };
