66a7da3472155a5195ff6c1d25ef6d92b545c99f
Alan Amalemba <alanlsamalemba@gmail.com>
2024-01-10 15:00:23 +0300

    Added client side adding and display of comments

--- client/src/pages/Post.tsx
+++ client/src/pages/Post.tsx
@@ -2,17 +2,26 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { PostType } from "./Home";
 
+type CommentType = {
+  commentBody: string;
+};
+
 export default function Post() {
   const { id } = useParams();
-  const [postObject, setPostObject] = useState<PostType>({});
+  const [postObject, setPostObject] = useState<PostType | null>(null);
+  const [commentsObject, setCommentsObject] = useState<CommentType[]>([]);
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     async function fetchProduct() {
       try {
         const response = await fetch(`http://localhost:3001/posts/byId/${id}`);
         const data = await response.json();
-        console.log(data);
         setPostObject(data);
+
+        const res = await fetch(`http://localhost:3001/comments/${id}`);
+        const comments = await res.json();
+        setCommentsObject(comments);
       } catch (error) {
         if (error instanceof Error) {
           console.log(error.message);
@@ -22,18 +31,65 @@ export default function Post() {
 
     fetchProduct();
   }, [id]);
+
+  async function addComment(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`http://localhost:3001/comments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ commentBody: comment, PostId: id }),
+      });
+
+      console.log("response ", response);
+
+      const data = await response.json();
+
+      console.log("Comment added: ", data);
+      setCommentsObject((comments) => [...comments, data]);
+      setComment("");
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+  }
+
   return (
     <div className="p-2 flex gap-2">
       <div className=" h-[200px] grow border flex flex-col  shadow  rounded bg-blue-500">
-        <div className="p-2 text-white text-center">{postObject.title}</div>
+        <div className="p-2 text-white text-center">{postObject?.title}</div>
         <div className="bg-white grow flex items-center justify-center text-sm ">
-          {postObject.postText}
+          {postObject?.postText}
         </div>
-        <div className="p-2 text-white">{postObject.username}</div>
+        <div className="p-2 text-white">{postObject?.username}</div>
       </div>
 
-      <div className="flex grow justify-center items-center">
-        <p className="text-center">Comment section</p>
+      <div className="flex flex-col gap-2 grow justify-center items-center">
+        <form className="flex flex-col gap-2" onSubmit={(e) => addComment(e)}>
+          <input
+            className="border shadow"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            required
+            type="text"
+          />
+          <button className="bg-blue-300 rounded">Add comment</button>
+        </form>
+
+        <div className="flex flex-col gap-1">
+          {commentsObject.map((comment, index) => (
+            <div
+              className="rounded rounded-tl-none p-1 bg-blue-300"
+              key={index}
+            >
+              {comment.commentBody}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
